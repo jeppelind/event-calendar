b0ec9670443de8376e05739fcc515eee9d08cb5a
Jesper Lind <jesper.lind@chyronhego.com>
2020-09-16 23:00:07 +0200

    Add test coverage check for db.

--- backend/db/src/mongo-connection.ts
+++ backend/db/src/mongo-connection.ts
@@ -3,34 +3,27 @@ import { model, connect } from "mongoose";
 
 const EventModel = model('Event', eventSchema);
 
-connect('mongodb://localhost:27017/event_calendar', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+export const createConnection = () => {
+  connect('mongodb://localhost:27017/event_calendar', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+}
 
 export const createEvent = async (name: string, startDate: string, endDate?: string, description?: string) => {
-  try {
-    const document = await EventModel.create({
-      name,
-      description,
-      startDate: new Date(startDate),
-      endDate: (endDate) ? new Date(endDate) : new Date(startDate),
-    });
-    return document;
-  } catch(err) {
-    console.error(err);
-    throw err;
-  }
+  const document = await EventModel.create({
+    name,
+    description,
+    startDate: new Date(startDate),
+    endDate: (endDate) ? new Date(endDate) : new Date(startDate),
+  });
+  return document;
 }
 
 export const deleteEvent = async (id: string) => {
-  try {
-    const result = await EventModel.deleteOne({ _id: id });
-    return result;
-  } catch(err) {
-    console.error(err);
-  }
+  const result = await EventModel.deleteOne({ _id: id });
+  return result;
 }
 
 export const updateEvent = async (id: string, name: string, startDate: string, endDate?: string, description?: string) => {
